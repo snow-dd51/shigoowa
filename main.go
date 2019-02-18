da594a6883fae1ad1fd077e44b05d849eba2a3a8
snow <snow821b@gmail.com>
2019-02-19 00:02:39 +0900

    add anaconda

--- main.go
+++ main.go
@@ -1,43 +1,71 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/ChimeraCoder/anaconda"
+	"io/ioutil"
 	"time"
 )
 
 const (
-	TimeStampFormat = "2006-01-02 15:04:05"
+	timeStampFormat = "2006-01-02 15:04:05"
+	confPath        = "./auth.json"
 )
 
 func main() {
-	mainLoop(NewMainLoopConf())
+	app, err := NewApp(confPath)
+	if err != nil {
+		return
+	}
+	app.mainLoop()
 }
 
-type MainLoopConf struct {
+type App struct {
 	IsDebug      bool
 	SleepSeconds int
+	TwAPI        *anaconda.TwitterApi
 }
 
-func mainLoop(c MainLoopConf) {
-	if !c.validate() {
+type AppConf struct {
+	AccessToken    string `json:"access-token"`
+	AccessSecret   string `json:"access-secret"`
+	ConsumerKey    string `json:"consumer-key"`
+	ConsumerSecret string `json:"consumer-secret"`
+}
+
+func NewApp(confpath string) (*App, error) {
+	content, err := ioutil.ReadFile(confpath)
+	if err != nil {
+		return nil, err
+	}
+	conf := AppConf{}
+	err = json.Unmarshal(content, &conf)
+	if err != nil {
+		return nil, err
+	}
+	api := anaconda.NewTwitterApiWithCredentials(conf.AccessToken, conf.AccessSecret, conf.ConsumerKey, conf.ConsumerSecret)
+	return &App{
+		IsDebug:      true,
+		SleepSeconds: 3,
+		TwAPI:        api,
+	}, nil
+}
+
+func (app *App) mainLoop() {
+	if !app.validateConf() {
 		return
 	}
 	ln := 0
 	for true {
-		fmt.Printf("[%s] Loop %d\n", time.Now().Format(TimeStampFormat), ln)
+		fmt.Printf("[%s] Loop %d\n", time.Now().Format(timeStampFormat), ln)
 		ln++
-		time.Sleep(time.Duration(c.SleepSeconds) * time.Second)
+		time.Sleep(time.Duration(app.SleepSeconds) * time.Second)
 	}
 }
 
-func NewMainLoopConf() MainLoopConf {
-	return MainLoopConf{
-		IsDebug:      true,
-		SleepSeconds: 3,
-	}
-}
-func (c MainLoopConf) validate() bool {
-	if c.SleepSeconds < 0 {
+func (app *App) validateConf() bool {
+	if app.SleepSeconds < 0 {
 		return false
 	}
 	return true
